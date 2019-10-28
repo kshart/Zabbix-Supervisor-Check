29c2ac250a725b3d806fc89e57a0b446c99e9472
Artyom Anatolьevich <kshart@yandex.ru>
2019-10-28 13:35:08 +0500

    Экранирование символа ':'

--- supervisor_check.pl
+++ supervisor_check.pl
@@ -8,58 +8,61 @@ my $ZabbixServer = "1.2.3.4";
 my $HostName = "HostName";
 # ----------------------------------
 
+sub escapeZabbixItemKey
+{
+    my $itemKey = $_[0];
+    $itemKey =~ s/:/_/g;
+    return $itemKey;
+}
+
 switch ($ARGV[0])
 {
-case "discovery" {
-my $first = 1;
+    case "discovery" {
+        my $first = 1;
 
-print "{\n";
-print "\t\"data\":[\n\n";
+        print "{\n";
+        print "\t\"data\":[\n\n";
 
-
-my $result = `/usr/bin/supervisorctl status`;
-
-my @lines = split /\n/, $result;
-foreach my $l (@lines) {
-        my @stat = split / +/, $l;
-#        my $status = substr($stat[1], 0, -1);
-
-                print ",\n" if not $first;
-                $first = 0;
-
-                print "\t{\n";
-                print "\t\t\"{#NAME}\":\"$stat[0]\",\n";
-                print "\t\t\"{#STATUS}\":\"$stat[1]\"\n";
-                print "\t}";
-}
-
-print "\n\t]\n";
-print "}\n";
-}
-
-case "status" {
-my $result = `/usr/bin/supervisorctl pid`;
-
-if ( $result =~ m/^\d+$/ ) {
-        $result = `/usr/bin/zabbix_sender -z $ZabbixServer -s $HostName -k "supervisor.status" -o "OK"`;
-        print $result;
-
-        $result = `/usr/bin/supervisorctl status`;
+        my $result = `/usr/bin/supervisorctl status`;
 
         my @lines = split /\n/, $result;
         foreach my $l (@lines) {
-                my @stat = split / +/, $l;
+            my @stat = split / +/, $l;
+            print ",\n" if not $first;
+            $first = 0;
+            my $name = escapeZabbixItemKey($stat[0]);
 
-                $result = `/usr/bin/zabbix_sender -z $ZabbixServer -s $HostName -k "supervisor.check[$stat[0],Status]" -o $stat[1]`;
-                print $result;
+            print "\t{\n";
+            print "\t\t\"{#NAME}\":\"$name\",\n";
+            print "\t\t\"{#STATUS}\":\"$stat[1]\"\n";
+            print "\t}";
         }
-}
-else {
-        # error supervisor not runing
-        $result = `/usr/bin/zabbix_sender -z $ZabbixServer -s $HostName -k "supervisor.status" -o "FAIL"`;
-        print $result;
-}
 
+        print "\n\t]\n";
+        print "}\n";
+    }
 
-}
+    case "status" {
+        my $result = `/usr/bin/supervisorctl pid`;
+
+        if ( $result =~ m/^\d+$/ ) {
+            $result = `/usr/bin/zabbix_sender -z $ZabbixServer -s $HostName -k "supervisor.status" -o "OK"`;
+            print $result;
+
+            $result = `/usr/bin/supervisorctl status`;
+
+            my @lines = split /\n/, $result;
+            foreach my $l (@lines) {
+                my @stat = split / +/, $l;
+                my $name = escapeZabbixItemKey($stat[0]);
+
+                $result = `/usr/bin/zabbix_sender -z $ZabbixServer -s $HostName -k "supervisor.check[$name,Status]" -o $stat[1]`;
+                print $result;
+            }
+        } else {
+            # error supervisor not runing
+            $result = `/usr/bin/zabbix_sender -z $ZabbixServer -s $HostName -k "supervisor.status" -o "FAIL"`;
+            print $result;
+        }
+    }
 }
